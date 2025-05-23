d201b027ca688671a9d445b596033dbed4242563
frkngl <frknglww@gmail.com>
2025-05-24 00:01:06 +0300

    PROJEDE VERİ TABANI BAGLANTISI YAPILDI VE ANASAYFADA RANDOM BLOGLAR GETIRME, YENI GELEN BLOGLARI ILK 10 GOSTERME, BLOG SAYISINA GORE POPULER ANIMELERI GOSTERME ILK 6 SEKILDE SON OLARAK GORUNTULENMEYE GORE POPULER BLOGLARI GOSTERME BURASI GORUNTULENME OLAYI HENUZ YAPILMADI BIR SONRAKI COMMITDE YAPILACAK.

--- GülBlogMVC5/Controllers/HomeController.cs
+++ GülBlogMVC5/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GülBlogMVC5.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,19 @@ namespace GülBlogMVC5.Controllers
 {
     public class HomeController : Controller
     {
+        GulBlogMVC5Entities db = new GulBlogMVC5Entities();
+        Data veri = new Data();
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            if (db == null && veri == null)
+            {
+                return View();
+            }
+
+            veri.Blog = db.TBLBLOGS.Where(x=>x.STATUS == true).ToList();
+            veri.Category = db.TBLCATEGORY.Where(x=>x.STATUS == true).ToList();
+            return View(veri);
         }
     }
 }
---
+++ GülBlogMVC5/Models/Data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GülBlogMVC5.Models
+{
+    public class Data
+    {
+        public List<TBLBLOGS> Blog { get; set; }
+        public List<TBLCATEGORY> Category { get; set; }
+    }
+}
